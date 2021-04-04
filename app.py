eee66afc2f8cdac90dacd42df2d9f28a0ca1bf27
flance3032021 <81904813+flance3032021@users.noreply.github.com>
2021-04-04 21:24:39 +0530

    Update app.py

--- app.py
+++ app.py
@@ -13,31 +13,34 @@ st.markdown(hide_streamlit_style, unsafe_allow_html=True)
 
 total_price= 0
 
-private_dyna= st.number_input('Enter number of Private Dyna', min_value= 0, max_value= 100, step= 1)
-if st.checkbox('I need insurance.', key= '1'):
-  private_dyna_price_per_km= 1.7
+col3, col4= st.beta_columns(2)
+bedrooms= col3.number_input('Enter number of bedrooms', 0, 100, 1)
+if col3.checkbox('Assembly needed', key= '1'):
+  bedroom_price= 200
 else:
-  private_dyna_price_per_km= 1  
+  bedroom_price= 120
 
-dyna_bed_dyna= st.number_input('Enter number of Dyna/bed/Dyna', min_value= 0, max_value= 100, step= 1)
-st.checkbox('I need insurance.', key= '2')
-dyna_bed_dyna_price_per_km= 0.85
-
-lowbed= st.number_input('Enter number of Lowbed', min_value= 0, max_value= 100, step= 1)
-st.checkbox('I need insurance.', key= '3')
-lowbed_price_per_km= 0.85
-
-luxury= st.number_input('Enter number of Luxury', min_value= 0, max_value= 100, step= 1)
-if st.checkbox('I need insurance.', key= '4'):
-  luxury_price_per_km= 1.7
+kidrooms= col3.number_input('Enter number of kid rooms', 0, 100, 1)
+if col3.checkbox('Assembly needed', key= '2'):
+  kidroom_price= 150
 else:
-  luxury_price_per_km= 1  
+  kidroom_price= 100
 
-motocycle= st.number_input('Enter number of Motocycle', min_value= 0, max_value= 100, step= 1)
-if st.checkbox('I need insurance.', key= '5'):
-  motocycle_price_per_km= 1.7
+livingrooms= col4.number_input('Enter number of living rooms', 0, 100, 1)
+if col4.checkbox('Assembly needed', key= '3'):
+  livingroom_price= 250
 else:
-  motocycle_price_per_km= 1  
+  livingroom_price= 140
+
+kitchens= col4.number_input('Enter number of kitchens', 0, 100, 1)
+if col4.checkbox('Assembly needed', key= '4'):
+  kitchen_price= 500
+else:
+  kitchen_price= 100
+
+ac_windows= st.number_input('Enter number of AC wndows', 0, 100, 1)
+acs= st.number_input('Enter number of ACs', 0, 100, 1)
+floors= st.number_input('Enter number of floors', 0, 100, 1)
 
 col1, col2= st.beta_columns(2)
 time1= col1.time_input('Order time : ')
@@ -53,44 +56,13 @@ try:
   loc2= geolocator.geocode(place2)
   lat_lon1= (loc1.latitude, loc1.longitude)
   lat_lon2= (loc2.latitude, loc2.longitude)
-  private_dyna_distt= geodesic(lat_lon1, lat_lon2).kilometers
-  dyna_bed_dyna_distt= private_dyna_distt
-  lowbed_distt= private_dyna_distt
-  luxury_distt= private_dyna_distt
-  motocycle_distt= private_dyna_distt
+  distt= geodesic(lat_lon1, lat_lon2).kilometers  
   
-  st.write(f'The distance is {round(private_dyna_distt, 2)} kilometres.')
+  st.write(f'The distance is {round(distt, 2)} kilometres.')
 
-  if private_dyna_distt>50.0:
-    total_price= total_price+(((private_dyna_distt*private_dyna_price_per_km)+100)*private_dyna)
-  else:
-    total_price= total_price+(150*private_dyna)
-
-  if dyna_bed_dyna_distt>300.0:    
-    total_price= total_price+(((dyna_bed_dyna_distt*dyna_bed_dyna_price_per_km)+300)*dyna_bed_dyna)    
-  else:
-    total_price= total_price+(0*dyna_bed_dyna)
-  
-  if lowbed_distt>300.0:
-    total_price= total_price+(((lowbed_distt*lowbed_price_per_km)+300)*lowbed)
-  else:
-    total_price= total_price+(0*lowbed)
-
-  if luxury_distt>149.0:
-    total_price= total_price+((((luxury_distt*luxury_price_per_km)+(750000*0.01)))*luxury)
-  else:
-    total_price= total_price+(((luxury_distt*luxury_price_per_km)+(750000*0.005))*luxury)
-
-  if motocycle_distt>50.0:
-    total_price= total_price+(((motocycle_distt*motocycle_price_per_km)+100)*motocycle)
-  else:
-    total_price= total_price+(150*motocycle)
+  total_price= (bedroom_price*bedrooms)+(kidroom_price*kidrooms)+(livingroom_price*livingrooms)+(kitchen_price*kitchens)+(ac_windows*50)+(acs*200)+(floors*50)+(distt*2)
 
   st.subheader(f'Total cost of transportation : {round(total_price, 2)}')  
   st.number_input('Insert your price')
-
 except:
   pass
-
-
-
